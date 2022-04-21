cae9a2d023a0e4f10f3fc56160c27536425b599f
Brancys <62186502+Brancys@users.noreply.github.com>
2022-04-21 08:20:49 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+import math
+
 class Basket: 
     def __init__(self):
         self.radius: float
@@ -11,6 +13,7 @@ class Ball:
     def __init__(self):
         self.radius: float
         self.weight: float
+        self.speed: float
     
 
 class Point:
@@ -38,10 +41,34 @@ class Game:
         self.gamePlayer: Player
         self.gameBall: Ball
         self.gravity: float
+        self.launchingAngle: float
+        self.t: float
      
     def getDataFromUser(self):
-        self.gamePlayer.height = raw_input("Digite Velocidad inicial del balon: ")
-        y0 = raw_input("Digite Altura del Jugador: ")
-        gravity = raw_input("Digite La gravedad del planeta: ")*-1
-        angle = raw_input("Digite el angulo del lanzamiento: ")
+        self.gameBall.speed =raw_input("Digite Velocidad inicial del balon: ")
+        self.gamePlayer.height = raw_input("Digite Altura del Jugador: ")
+        self.gravity = raw_input("Digite La gravedad del planeta: ")*-1
+        self.launchingAngle = raw_input("Digite el angulo del lanzamiento en grados: ")
+        self.t = raw_input("Digite el momento t en segundos a consultar:")
+        
+
+input_test = Game()
+ball_test = Ball()
+player_test = Player()
+input_test.gameBall = ball_test
+input_test.gamePlayer = player_test
+
+input_test.getDataFromUser()
+print("Datos ingresados:")
+print("Velocidad inicial (m/s):" +input_test.gameBall.speed)
+print("Altura inicial (m):" +input_test.gamePlayer.height)
+print("Gravedad del lugar (m/s^2):" +input_test.gravity)
+print("Angulo de lanzamiento (º):" +input_test.angle)
+print("Momento a revisar (s):" +input_test.gameBall.speed)
+print("Datos obtenidos correctamente")
+
+
+
+
+        
             
